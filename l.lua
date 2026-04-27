93b515073e1c07daf5d21fea207440311dd7d9f0
inkwellblogs <inkwellblog0@gmail.com>
2026-04-27 16:28:26 +0530

    Delete l.lua

--- l.lua
+++
@@ -1,5 +0,0 @@
-local postRemote = game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("POST")
-local refill = workspace.Unclimbable.Props.HQ.GasTank.Refill
-print("Refill part:", refill)
-postRemote:FireServer("Attacks", "Reload", refill)
-print("Fired!")
