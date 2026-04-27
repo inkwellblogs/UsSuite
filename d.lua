625d99c01460603df85b7259159dbd230d6d706b
inkwellblogs <inkwellblog0@gmail.com>
2026-04-27 16:30:19 +0530

    Delete d.lua

--- d.lua
+++
@@ -1,5 +0,0 @@
-local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
-local top = PlayerGui.Interface.HUD.Main.Top
-for _, v in ipairs(top:GetChildren()) do
-    print(v.Name, v.ClassName)
-end
