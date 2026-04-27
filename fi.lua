4f0cea5fd12859a8252fa9522422638c533acc29
inkwellblogs <inkwellblog0@gmail.com>
2026-04-27 16:30:09 +0530

    Delete fi.lua

--- fi.lua
+++
@@ -1,9 +0,0 @@
-local PlayerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
-local top = PlayerGui.Interface.HUD.Main.Top
-for _, frame in ipairs(top:GetChildren()) do
-    if frame:IsA("Frame") then
-        for _, child in ipairs(frame:GetChildren()) do
-            print(frame.Name, "->", child.Name, child.ClassName, pcall(function() return child.Text end))
-        end
-    end
-end
