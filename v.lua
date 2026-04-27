53e019f65c58202aef4372ef02c17ab8391aa987
inkwellblogs <inkwellblog0@gmail.com>
2026-04-27 16:26:56 +0530

    Delete v.lua

--- v.lua
+++
@@ -1,7 +0,0 @@
-local postRemote = game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Remotes"):WaitForChild("POST")
-local refill = workspace.Unclimbable:FindFirstChild("Refill", true)
-print("Refill found:", refill)
-if refill then
-    postRemote:FireServer("Attacks", "Reload", refill)
-    print("Fired!")
-end
